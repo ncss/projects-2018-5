2f0eaeb36823a8cb2d6b673296717c4fdfb18a01
Alex <defaultnamehere+ncss-2018@gmail.com>
2018-01-11 12:00:43 +1100

    Add cool styles to our homepage
    Changed "my dudes" to "fellow students" and added a harder-to-see
    colour.

--- ncssbook.py
+++ ncssbook.py
@@ -3,7 +3,7 @@ from tornado.ncss import Server
 
 
 def index(response):
-    response.write('<h1>Hello my dudes</h1><p>from <span style="color: red;">Group 5</span></p>')
+    response.write('<h1>Hello fellow students</h1><p>from <span style="color: #cccccc;">Group 5</span></p>')
 
 
 server = Server()
