dbbf776d66e685f7efcdf1f7ce51ebad9940d390
john2day <jmcgerty@gmail.com>
2018-01-11 12:10:18 +1100

    Added image of James
    Added picture of James from Grok Learning's About Page

--- ncssbook.py
+++ ncssbook.py
@@ -3,7 +3,7 @@ from tornado.ncss import Server
 
 
 def index(response):
-    response.write('<h1>Hello fellow students</h1><p>from <span style="color: #cccccc;">Group 5</span></p>')
+    response.write('<h1>Hello fellow students</h1><p>from <span style="color: #cccccc;">Group 5</span></p><img src="https://static.groklearning-cdn.com/static/images/marketing/about/james-2017-07@2x.png">')
 
 
 server = Server()
