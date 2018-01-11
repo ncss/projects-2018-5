938387cf8d47784e7aa8146b62ac5f643feea61b
sophieclaes <sophieclaes18@gmail.com>
2018-01-11 17:25:18 +1100

    add profile page

--- ncssbook.py
+++ ncssbook.py
@@ -4,8 +4,12 @@ from tornado.ncss import Server
 
 def index(response):
     response.write('<h1>Hello fellow students</h1><p>from <span style="color: #cccccc;">Group 5</span></p>')
+def profile(response):
+    with open('templates/profile_page.html') as f:
+        response.write(f.read())
 
 
 server = Server()
 server.register('/', index)
+server.register('/profile', profile)
 server.run()
