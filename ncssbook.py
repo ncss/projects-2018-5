f31b1232e64b0dfe343bcf4b8a58565fe1397b43
Jessie A <jaustin042000@gmail.com>
2018-01-12 12:06:15 +1100

    Fleshed out stlye guide

--- ncssbook.py
+++ ncssbook.py
@@ -11,16 +11,36 @@ def style_guide(response):
     response.write('''
 <!DOCTYPE html>
 <head>
-<link href="/static/css/styleguide.css" rel="stylesheet"/>
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,400i,600,800" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="/static/css/styleguide.css" rel="stylesheet"/>
 </head>
 <body>
-<h1> Style Guide </h1>
-<a href="/">Home</a>
+    <header>
+        <h1> Style Guide </h1>
+        <a href="/">Home</a>
+    </header>
+    <h1>This is a level 1 heading.</h1>
+    <h2>This is a level 2 heading.</h2>
+    <h3> This is a level 3 heading.</h3>
+    <p>This is a paragraph.</p>
+    <ol>
+        <li>Song 1</li>
+        <li>Song number 2</li>
+        <li>Song #3</li>
+        <li>Fourth Song</li>
+    </ol>
 
-<h1>This is a level 1 heading.</h1>
-<h2>This is a level 2 heading.</h2>
-<p>This is a paragraph.</p> 
-
+    <ul>
+        <li>Tim</li>
+        <li>James</li>
+        <li>Nicky</li>
+        <li>Bruce</li>
+    </ul>
+    <i class="material-icons mats">thumb_down</i>
+    <i class="material-icons">thumb_up</i>
+    <i class="material-icons">pause</i>
+    <i class="material-icons">play_arrow</i>
 </body>
 
 
