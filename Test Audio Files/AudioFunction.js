9dc7938939884f12ed7ac71b3e13737a5e0492aa
jasonyu0100 <jasonyu0100@gmail.com>
2018-01-13 11:44:51 +1100

    song snippets implemented

--- Test Audio Files/AudioFunction.js
+++
@@ -1,9 +0,0 @@
-playSong = function() {
-  let audio = document.getElementById('audioPlayer');
-  let songLength = audio.length;
-  let start = songLength / 2;
-  audio.currentTime = start;
-  audio.play();
-}
-
-playSong();
