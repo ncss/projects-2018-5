57bb6be63e28148b7b9408e3d8d7950225cc9947
jasonyu0100 <jasonyu0100@gmail.com>
2018-01-13 15:28:38 +1100

    Added skipping functoin for audio :)
    works and is cool

--- static/js/getsnippet.js
+++ static/js/getsnippet.js
@@ -1,30 +1,47 @@
+let filePath = '/static/mp3/'
 let songs;
+let songCount = 0
+let audio = document.getElementById('audioPlayer');
+
+function playNextSnippet() {
+  audio.currentTime = (audio.duration/2) - 5;
+  audio.play();
+  setTimeout(function(){
+    seekNextSong();
+  },10000);
+}
+
+function seekNextSong() {
+  audio.pause();
+  songCount += 1;
+  let nextSong = songs[songCount];
+  changeAudioPath(nextSong.title);
+  if (audio.readyState >= 1) {
+    playNextSnippet();
+  } else {
+    audio.addEventListener("loadeddata", playNextSnippet);
+  }
+}
+
+function changeAudioPath(songName) {
+  audio.src = filePath + songName + '.mp3';
+}
 
 function getSongs() {
   fetch('/songdb').then(function(response){
     return response.json()
   }).then(function(data){
     songs = data;
+  }).then(function() {
+    let nextSong = songs[songCount];
+    changeAudioPath(nextSong.title);
+    let audio = document.getElementById('audioPlayer');
+    if (audio.readyState >= 1) {
+      playNextSnippet();
+    } else {
+      audio.addEventListener("loadeddata", playNextSnippet);
+    }
   });
 }
 
 getSongs();
-
-function playNextSnippet() {
-  console.log(songs);
-  let audio = document.getElementById('audioPlayer');
-  audio.currentTime = (audio.duration/2) - 5;
-  audio.play();
-  setTimeout(function(){
-    audio.pause();
-    //Go to next song or if like or dislike
-    // Audio();
-  },10000);
-}
-
-let audio = document.getElementById('audioPlayer');
-if (audio.readyState >= 1) {
-  playNextSnippet();
-} else {
-  audio.addEventListener("load", playNextSnippet);
-}
